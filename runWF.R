710910e3ebb826f00f154612516cf940f93ee2ab
Michael Sevestre <michael@design2code.ca>
2022-04-04 13:43:20 -0400

    Update template for qualification #1

---
+++ runWF.R
@@ -0,0 +1,21 @@
+rm(list = ls())
+library(ospsuite.reportingengine)
+workingDirectory <- getwd()
+reInputFolder <- file.path(workingDirectory, "re_input")
+reOutputFolder <- file.path(workingDirectory, "re_output")
+configurationPlanName <- "report-configuration-plan"
+configurationPlanFile <- file.path(reInputFolder, paste0(configurationPlanName, ".json"))
+recordWorkflowTime <- TRUE
+workflow <- loadQualificationWorkflow(
+  workflowFolder = reOutputFolder,
+  configurationPlanFile = configurationPlanFile
+)
+workflow$inactivateTasks("simulate")
+workflow$activateTasks("plotTimeProfiles")
+workflow$inactivateTasks("calculatePKParameters")
+workflow$inactivateTasks("plotDDIRatio")
+workflow$inactivateTasks("plotComparisonTimeProfile")
+
+tictoc::tic()
+workflow$runWorkflow()
+tictoc::toc()
